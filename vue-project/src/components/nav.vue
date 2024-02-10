964c1a68537bc612e2226ae36de5082a71b1abdd
Dharmik sarvaiya <155711667+Dharmiksarvaiya24@users.noreply.github.com>
2024-02-10 16:32:59 +0530

    Update nav.vue

--- vue-project/src/components/nav.vue
+++ vue-project/src/components/nav.vue
@@ -2,7 +2,7 @@
   <div class="nav">
       <nav>
           <ul>
-          <li><a href="this.$router.push('/');">Home</a></li>
+          <li><a href=this.$router.push('/');>Home</a></li>
           <li class="center">User Data Management</li>
           </ul>
       </nav>
